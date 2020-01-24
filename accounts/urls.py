8ed3b9f3a427f559ae7b4f2450009c1177042e87
peteballs <gsinvestments.prh@gmail.com>
2020-01-23 19:37:56 -0600

    update

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login, name='login'),
+    path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
     path('dashboard', views.dashboard, name='dashboard')
