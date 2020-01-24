dddd17f481ec4b1cdd9a6d3333a7f4ae5147b1d0
peteballs <gsinvestments.prh@gmail.com>
2020-01-23 20:59:40 -0600

    update

--- Automated_Investments/urls.py
+++ Automated_Investments/urls.py
@@ -4,8 +4,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', include('pages.urls')),
-    path('listings/', include('listings.urls')),
+    path('', include('static_web_pages.urls')),
+    # path('listings/', include('listings.urls')),
     path('accounts/', include('accounts.urls')),
     path('admin/', admin.site.urls),
 ] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
