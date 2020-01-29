6e457901e3343f3d94087260bae650adb51c6294
peteballs <gsinvestments.prh@gmail.com>
2020-01-28 21:51:20 -0600

    update

--- calculations/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CalculationsConfig(AppConfig):
-    name = 'calculations'
--- static_web_pages/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StaticWebPagesConfig(AppConfig):
-    name = 'static_web_pages'
